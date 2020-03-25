055a17c1255880e48fb97c37755e9e2ca5f8dc6b
v12345vtm <plekke@yahoo.com>
2020-03-25 13:51:15 +0100

    Add files via upload

---
+++ macros/hbm2labels/genereerstroomcalib.bat
@@ -0,0 +1,8 @@
+
+
+
+
+
+hbm2labels.exe \\file01.letdeinze.lan\company\Letdata\data\06\0011_00\D1\060011_00-D1N32F.xlsx -o "stroomcalib.txt"
+
+\\file01.letdeinze.lan\company\Letdata\data\06\0011_00\D1\060011_00-D1N32F.xlsx
