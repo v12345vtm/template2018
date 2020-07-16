0e4bdc8916d555e8e6b53515a1dacb6fc00284ff
v12345vtm <plekke@yahoo.com>
2020-07-16 13:06:49 +0200

    Add files via upload

--- macros/hbm2labels/genereerstroomcalib.bat
+++ macros/hbm2labels/genereerstroomcalib.bat
@@ -1,8 +1,13 @@
+rem "dit programma komt van pablo , en extract vanuit excel alles naar txt voor clipproject , in cipproject import nr32 stroomcandela voor je labels te zien"
+rem  \\file01.letdeinze.lan\company\Letdata\data\06\0011_00\D1\060011_00-D1N32F.xlsx
+set  excel=\\file01.letdeinze.lan\company\Letdata\data\06\0011_00\D1\060011_00-D1N32G.xlsx
+echo %excel%
+ 
+
+pause
 
 
+hbm2labels.exe %excel% -o "stroomcalib.txt"
 
-
-
-hbm2labels.exe \\file01.letdeinze.lan\company\Letdata\data\06\0011_00\D1\060011_00-D1N32F.xlsx -o "stroomcalib.txt"
-
-\\file01.letdeinze.lan\company\Letdata\data\06\0011_00\D1\060011_00-D1N32F.xlsx
+rem open excelbestand
+%excel%  
